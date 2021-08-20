4571f892e2b96829f5f595085cdc44d69a344b14
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-19 20:00:59 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,19 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    
+    if edad >= 18:
+        per=str(input("¿Tienes identificación oficial? (s/n): "))
+        if per == "s":
+            print("Trámite de licencia concedido")
+        elif per=="n":
+            print("No cumples los requisitos")
+        else :
+            print("Respuesta incorrecta")
+    else :
+        print("No cumples los requisitos")
+
 
 
 if __name__ == '__main__':
