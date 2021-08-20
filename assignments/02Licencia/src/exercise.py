b8ef10ceadf2b49c0d123e4bfdd5b281b318f871
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-20 17:11:54 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,7 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    
     edad = int(input("Ingresa tu edad: "))
     
     if edad >= 18:
@@ -9,15 +10,14 @@ def main():
             print("Trámite de licencia concedido")
         elif per=="n":
             print("No cumples requisitos")
-        else :
+        else:
             print("Respuesta incorrecta")
             
-    elif edad <0 :
-        print("Respuesta incorrecta")  
-    else :
+    elif 0 < edad < 18 :
         print("No cumples requisitos")
 
-
-
+    else :
+        print("Respuesta incorrecta") 
+        
 if __name__ == '__main__':
     main()
